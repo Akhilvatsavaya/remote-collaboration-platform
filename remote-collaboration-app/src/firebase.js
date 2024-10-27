2f49ddb6cebf026dd985ab32d676f64dd0a66512
Akhil Varma <akhilvatsavaya@gmail.com>
2024-10-26 18:11:49 -0700

    Implemented Firebase Authentication with environment variables for secrets

--- remote-collaboration-app/src/firebase.js
+++ remote-collaboration-app/src/firebase.js
@@ -2,13 +2,13 @@
     import {getAuth} from "firebase/auth";
 
     const firebaseConfig = {
-      apiKey: "AIzaSyC1WUefNCevbSe5AWoQn7QEdFHuoWhnD1w",
-      authDomain: "remote-collaboration-platform.firebaseapp.com",
-      projectId: "remote-collaboration-platform",
-      storageBucket: "remote-collaboration-platform.appspot.com",
-      messagingSenderId: "25736081048",
-      appId: "1:25736081048:web:babe7b499952b8dea32aed",
-      measurementId: "G-8XDPM89S1X"
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID,
+      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
     };
 
     const app = initializeApp(firebaseConfig);
